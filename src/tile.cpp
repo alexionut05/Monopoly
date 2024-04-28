d20f324ade6e00e2973f155a01cd024dde2adb8c
alexionut05 <alexionut05@users.noreply.github.com>
2024-04-28 11:29:41 +0300

    Adjusted tile Class in preparation for breaking it up in multiple subclasses

--- src/tile.cpp
+++ src/tile.cpp
@@ -26,21 +26,6 @@ std::string Tile::GetOwner() const
 	return owner_;
 }
 
-int Tile::GetTilePrice() const
-{
-	return tile_price_;
-}
-
-int Tile::GetPropertyPrice() const
-{
-	return property_price_;
-}
-
-std::array<int, 7> Tile::GetRents() const
-{
-	return rents_;
-}
-
 // Setters
 void Tile::SetName(const std::string name)
 {
@@ -60,19 +45,4 @@ void Tile::SetColour(const std::string colour)
 void Tile::SetOwner(const std::string owner)
 {
 	owner_ = owner;
-}
-
-void Tile::SetTilePrice(const int tile_price)
-{
-	tile_price_ = tile_price;
-}
-
-void Tile::SetPropertyPrice(const int property_price)
-{
-	property_price_ = property_price;
-}
-
-void Tile::SetRents(const std::array<int, 7> rents)
-{
-	rents_ = rents;
 }
