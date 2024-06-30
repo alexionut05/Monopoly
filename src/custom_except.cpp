bb6e3deeb6761348657703503e74c4eef90fdfec
alexionut05 <alexionut05@users.noreply.github.com>
2024-06-30 12:44:58 +0300

    :bug: fix: Fix CI issues with custom_except.cpp

--- src/custom_except.cpp
+++ src/custom_except.cpp
@@ -3,10 +3,12 @@
 CustomException::CustomException(const std::string &message)
 	: message_(message) {}
 
-const char *CustomException::what() const noexcept
-{
-	return message_.c_str();
-}
+
+// Currently unused code
+// const char *CustomException::what() const noexcept
+// {
+// 	return message_.c_str();
+// }
 
 FileNotFound::FileNotFound(const std::string &filename)
 	: CustomException("Error: file not found: " + filename) {}
